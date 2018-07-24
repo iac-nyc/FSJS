f6b019e060c9a2e38e367a32361230432ba65aaa
iac-nyc <007ifte@gmail.com>
2018-07-24 07:56:55 -0400

    ES2015-initial

---
+++ ES2015/initial/let-loop-scope.js
@@ -0,0 +1,12 @@
+'use strict';
+
+(function initLoop() {
+  
+  function doLoop(x) {
+    console.log('loop:', x);
+  }
+  
+  for (let i = 0; i < 10; i++) {
+    doLoop(i + 1);
+  }
+})();
---
+++ ES2015/initial/var-scope.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var hello = 'hello iftekhar';
+
+function sayHi() {
+  var hello = 'hi';
+  console.log(hello);
+}
+
+sayHi();
+console.log(hello);
