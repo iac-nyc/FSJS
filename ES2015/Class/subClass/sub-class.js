337b0b5b99bb4d7ca6b8ac1ec9894cacdafed684
iac-nyc <007ifte@gmail.com>
2018-07-27 09:55:38 -0400

    subClass

---
+++ ES2015/Class/subClass/sub-class.js
@@ -0,0 +1,50 @@
+'use strict';
+
+class Person {
+  dance() {
+    const dances = [
+      'waltz',
+      'tango',
+      'mambo',
+      'foxtrot'
+    ];
+    console.log(`${this.name} is doing the ${dances[Math.floor(Math.random() * dances.length)]}!`);
+  }
+  constructor({ name, age, eyeColor = 'brown' } = {}) {
+    this.name = name;
+    this.age = age;
+    this.eyeColor = eyeColor;
+  }
+}
+
+class Student extends Person {
+  dance(traditional){
+    if(traditional){    
+      super.dance();
+      return;
+    }else{
+       const dances = [
+      'lyrical',
+      'tap',
+      'ballet',
+      'jazz'
+    ];
+      console.log(`${this.name} is doing the ${dances[Math.floor(Math.random() * dances.length)]}!`);
+    
+    }
+    
+  }  
+  
+  constructor({ name, age, interestLevel = 5 } = {} ) {
+    super({name, age});
+    this.name = name;
+    this.age = age;
+    this.interestLevel = interestLevel;
+    this.grades = new Map;
+  }
+}
+
+let stevenJ = new Student({ name: 'Steven', age: 22 , interestLevel:8});
+stevenJ.dance();
+console.log(stevenJ.interestLevel);
+
