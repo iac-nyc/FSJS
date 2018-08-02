179bd4e379decc38ccce42e12000402bec0df946
iac-nyc <007ifte@gmail.com>
2018-08-01 21:09:47 -0400

    JSON_parse

---
+++ Node.js/JSON_parse.js
@@ -0,0 +1,4 @@
+const jsonString = '{"name": "Iftekhar", "languages": ["javascript", "css", "html", "design", "python", "jQuery", "bootstrap"]}';
+
+  const jsonObject = JSON.parse(jsonString);
+console.log(jsonObject);
