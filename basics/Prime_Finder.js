5e582a0572d6665961c490ddb08f5ebd5646162f
iac-nyc <007ifte@gmail.com>
2018-09-15 09:11:12 -0400

    Prime_Finder

---
+++ basics/Prime_Finder.js
@@ -0,0 +1,30 @@
+<html>
+	<head>
+		<title>Javascript4#1</title>
+	</head>
+	<body>
+		<script type="text/javascript">
+		var a="prompt("Type number here.","");
+		var i=2;
+		var b;
+		var c="true";
+		while(i<a)
+		{
+			b=a/i;
+			if(b=0)
+			{
+				c="false";
+			}
+			1++;
+		}
+		if(c="false")
+		{
+			alert("Your number is not a prime number");
+		}
+		else
+		{
+			alert("Your number is a prime number");
+		}
+		</script>
+	</body>
+</html>
