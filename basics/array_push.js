7797b031990c96a801406f05a4fcd0e837a142fb
iac-nyc <007ifte@gmail.com>
2018-09-17 11:24:33 -0400

    array_push

---
+++ basics/array_push.js
@@ -0,0 +1,4 @@
+var donuts = ["glazed", "chocolate frosted", "Boston creme", "glazed cruller", "cinnamon sugar", "sprinkled"];
+donuts.push("powdered"); // the `push()` method returns 7 because the `donuts` array now has 7 elements
+
+
