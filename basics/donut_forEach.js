7c79e56db37fe98b3dc0ac8348f7ef6604e843e9
iac-nyc <007ifte@gmail.com>
2018-09-18 09:06:21 -0400

    donut_forEach

---
+++ basics/donut_forEach.js
@@ -0,0 +1,11 @@
+var donuts = ["jelly donut", "chocolate donut", "glazed donut"];
+
+donuts.forEach(function(donut) {
+  donut = "i love " + donut;
+  donut = donut.toUpperCase();
+  console.log(donut);
+});
+
+// I LOVE JELLY DONUT
+// I LOVE CHOCOLATE DONUT
+// I LOVE GLAZED DONUT
