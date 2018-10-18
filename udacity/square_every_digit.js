63db8533e9999e7d90e5c501586cde64f7b353cb
iac-nyc <007ifte@gmail.com>
2018-10-18 09:08:59 -0400

    square_every_digit

---
+++ udacity/square_every_digit.js
@@ -0,0 +1,17 @@
+/*
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+*/
+
+function squareDigits(num){
+  //may the code be with you
+var strNum = String(num);
+  var newNum ='';
+  
+  for (i=0; i<=strNum.length-1 ;i++){
+    newNum += strNum[i]*strNum[i];
+  }
+  
+  return Number(newNum);
+
+  
+}
