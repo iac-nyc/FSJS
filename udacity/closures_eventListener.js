45200cc5957352208e301a0260ef2e01b9e4a8e0
iac-nyc <007ifte@gmail.com>
2018-10-18 09:11:30 -0400

    closures_eventListener

---
+++ udacity/closures_eventListener.js
@@ -0,0 +1,25 @@
+// clear the screen for testing
+document.body.innerHTML = '';
+document.body.style.background="red";
+
+var nums = [1,2,3,4,5,6,7,8,9,10];
+
+// Let's loop over the numbers in our array
+for (var i = 0; i < nums.length; i++) {
+
+    // This is the number we're on...
+    var num = nums[i];
+
+    // We're creating a DOM element for the number
+    var elem = document.createElement('div');
+    elem.textContent = num;
+
+    // ... and when we click, alert the value of `num`
+    elem.addEventListener('click', (function(numCopy) {
+        return function() {
+            alert(numCopy);
+        };
+    })(num));
+
+    document.body.appendChild(elem);
+};
