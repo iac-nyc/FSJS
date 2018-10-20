1c73add1beed8ae2b4cc27a7e2bac8bbcaa894d4
iac-nyc <007ifte@gmail.com>
2018-10-20 10:45:53 -0400

    red_background

---
+++ udacity/red_background/app.js
@@ -0,0 +1,6 @@
+const warning = document.getElementById("warning");
+let button = document.getElementById('makeItRed');;
+button.addEventListener('click', function(){
+	warning.style.backgroundColor = 'red';
+});
+
