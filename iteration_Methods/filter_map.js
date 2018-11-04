9cad6e872453e20e5970f5ec92d3c35de85dcc0a
Iftekhar A Chowdhury <007ifte@gmail.com>
2018-11-04 09:12:30 -0500

    filter_map

---
+++ iteration_Methods/filter_map.js
@@ -0,0 +1,29 @@
+
+const todos = [
+    {
+        todo: 'Buy apples',
+        done: false
+    },
+    {
+        todo: 'Wash car',
+        done: true
+    },
+    {
+        todo: 'Write web app',
+        done: false
+    },
+    {
+        todo: 'Read MDN page on JavaScript arrays',
+        done: true
+    },
+    {
+        todo: 'Call mom',
+        done: false
+    }
+];
+let unfinishedTasks;
+
+// unfinishedTasks should be: ["Buy apples", "Write web app", "Call mom"]
+// Write your code below
+ unfinishedTasks = todos.filter( todos => todos.done ===false).map(todos => todos.todo);
+console.log(unfinishedTasks);
