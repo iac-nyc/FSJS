aec74b2bdb9205bc4a85339de7330866666420d9
Iftekhar A Chowdhury <007ifte@gmail.com>
2018-11-06 07:33:11 -0500

    spreadOperator_reduce

---
+++ iteration_Methods/spreadOperator_reduce.js
@@ -0,0 +1,11 @@
+const customerNames = [
+    [ "John", "Sandy", "Tyrone", "Elizabeth", "Penny" ],
+    [ "Barry", "Wanda", "Jamal", "Hayden" ],
+    [ "Portia", "Pam", "Philip" ]
+];
+let flattenedCustomerNames;
+
+// flattenedCustomerNames should be: ["John", "Sandy", "Tyrone", "Elizabeth", "Penny", "Barry", "Wanda", "Jamal", "Hayden", "Portia", "Pam", "Philip"]
+// Write your code below
+flattenedCustomerNames = customerNames.reduce((arr,names) => [...arr, ...names],[]);
+console.log(flattenedCustomerNames);
