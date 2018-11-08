8d15b44e9af99843e58d3e2934313b1d6fc52ca9
Iftekhar A Chowdhury <007ifte@gmail.com>
2018-11-08 06:40:06 -0500

    prime_finder_updated

--- basics/Prime_Finder.js
+++ basics/Prime_Finder.js
@@ -1,30 +1,17 @@
-<html>
-	<head>
-		<title>Javascript4#1</title>
-	</head>
-	<body>
-		<script type="text/javascript">
-		var a="prompt("Type number here.","");
-		var i=2;
-		var b;
-		var c="true";
-		while(i<a)
-		{
-			b=a/i;
-			if(b=0)
-			{
-				c="false";
-			}
-			1++;
+function isPrime(number){
+	if (number <=1){
+		return false;
+    }
+var idx=2;
+while(idx < number){
+	if(number % idx === 0){
+	 return false;
+
 		}
-		if(c="false")
-		{
-			alert("Your number is not a prime number");
-		}
-		else
-		{
-			alert("Your number is a prime number");
-		}
-		</script>
-	</body>
-</html>
+	idx +=1;
+
+	}
+	return true;
+
+}
+console.log(isPrime(13));
