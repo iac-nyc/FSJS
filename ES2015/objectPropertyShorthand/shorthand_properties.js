abe12246f0e6b4c0eca56c4da986ce9d37f4def7
Iftekhar A Chowdhury <007ifte@gmail.com>
2018-12-12 21:34:18 -0500

    shorthand_properties

---
+++ ES2015/objectPropertyShorthand/shorthand_properties.js
@@ -0,0 +1,25 @@
+// Method Properties
+const hello = {
+    name : 'abcdef',
+    printHello(){
+        console.log('Hello');
+    }
+}
+console.log(hello.printHello());
+
+/*
+'use strict';
+
+// Method Properties
+var hello = {
+    name: 'abcdef',
+    printHello: function printHello() {
+        console.log('Hello');
+    }
+};
+console.log(hello.printHello());
+
+
+
+
+*/
